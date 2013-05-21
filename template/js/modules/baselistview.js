2cb47c679937fc4e008527d907a4fe6a6513c464
José Roberto Vidal <rvidal@extrema-sistemas.com>
2013-05-21 15:29:58 -0400

    Clases genéricas de Backbone para trview y listview

---
+++ template/js/modules/baselistview.js
@@ -0,0 +1,32 @@
+define(
+  function() {
+
+    return B.View.extend({
+      events: {
+        'tap tr': function(e) {
+          window.location.hash = this.url +
+            $(e.target).closest('[data-id]').data('id');
+        }
+      },
+
+      initialize: function(options) {
+        this.url = options.url;
+        this.trView = options.trView;
+      },
+
+      render: function() {
+        var $tbody = $('<tbody></tbody>');
+        _.each(this.collection, function(model) {
+          $tbody.append(
+            (new this.trView({
+              model: model
+            })).render().$el
+          );
+        }, this);
+        this.$el.html($('<table></table>').append($tbody));
+        return this;
+      }
+    });
+
+  }
+);
---
+++ template/js/modules/basetrview.js
@@ -0,0 +1,14 @@
+define(
+  function() {
+    return B.View.extend({
+      el: function() {
+        return '<tr data-id="' + this.model.id + '"></tr>'
+      },
+
+      render: function() {
+        this.$el.html(this.tmpl(this.model))
+        return this;
+      },
+    });
+  }
+);
