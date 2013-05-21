55cb878056511cd2433116c4ab3a683364b5e99f
José Roberto Vidal <rvidal@extrema-sistemas.com>
2013-05-21 17:51:10 -0400

    Lint

--- template/js/db/loaddb.js
+++ template/js/db/loaddb.js
@@ -42,7 +42,7 @@ define(
         }
 
         persistence.flush(callback);
-      }
+      };
     };
 
     return function(locale, callback) {
--- template/js/modules/basetrview.js
+++ template/js/modules/basetrview.js
@@ -2,11 +2,11 @@ define(
   function() {
     return B.View.extend({
       el: function() {
-        return '<tr data-id="' + this.model.id + '"></tr>'
+        return '<tr data-id="' + this.model.id + '"></tr>';
       },
 
       render: function() {
-        this.$el.html(this.tmpl(this.model))
+        this.$el.html(this.tmpl(this.model));
         return this;
       },
     });
