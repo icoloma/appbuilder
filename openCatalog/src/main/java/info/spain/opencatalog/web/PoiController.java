528bf6efc390d73f1a377ec4026e57e20ded86d1
Ernesto Hdez <ehdez@extrema-sistemas.com>
2013-05-23 19:27:05 +0200

    Arreglada la paginación, la ordenación y el enlace a cada Poi

--- openCatalog/src/main/java/info/spain/opencatalog/web/PoiController.java
+++ openCatalog/src/main/java/info/spain/opencatalog/web/PoiController.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefaults;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,17 +19,25 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * Handles requests for the application poi page.
  */
 @Controller
+@RequestMapping(value = "/admin/poi")
 public class PoiController {
 	
 	@Autowired
 	private PoiRepository poiRepository;
 
-	@RequestMapping(value = "/admin/poi", method = RequestMethod.GET)
-	public String home(Locale locale, Model model, @PageableDefaults Pageable pageable) {
+	@RequestMapping(method = RequestMethod.GET)
+	public String list(Model model, @PageableDefaults(sort="name.es") Pageable pageable) {
 		Page<Poi>page  = poiRepository.findAll(pageable);
 		model.addAttribute("page_title","poi.title");
 		model.addAttribute("page", page);
 		return "admin/poi/poiList";
 	}
 
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	public String show(@PathVariable("id") String id, Model model, @PageableDefaults(sort="name.es") Pageable pageable) {
+		Poi poi  = poiRepository.findOne(id);
+		model.addAttribute("page_title","poi.title");
+		model.addAttribute("poi", poi);
+		return "admin/poi/poi";
+	}
 }
--- openCatalog/src/main/webapp/resources/js/main.js
+++ openCatalog/src/main/webapp/resources/js/main.js
@@ -1,1 +1,27 @@
+$(function(){
+	
+	// table.poi : add links to Pois
+	$("table.poi tbody tr").each( function(){
+		$(this).click( function(){
+			window.location= $(this).data("link");
+		} );
 
+	});
+	
+	// pager
+	$("ul.pager").each( function(){
+		var page = $(this).data("page");
+		var pages = $(this).data("pages");
+		if (page<1){
+			// disble prev button and link
+			$("li.previous", this).addClass("disabled");
+			$("li.previous a",this).attr("href","#").click(function(){return false;});
+		}
+		if (page >= pages -1){
+			// disble last button and link
+			$("li.next", this).addClass("disabled");
+			$("li.next a",this).attr("href","#").click(function(){return false;});
+		}
+	});
+	
+});
