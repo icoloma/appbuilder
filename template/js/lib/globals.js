f82a27b53afb0ddb1ab6bc263a881e1808d1c591
José Roberto Vidal <rvidal@extrema-sistemas.com>
2013-05-28 17:06:11 -0400

    Custom build de async

--- template/js/lib/globals.js
+++ template/js/lib/globals.js
@@ -2,6 +2,7 @@ define(
   [ 
     'lib/jquery', 'lib/underscore', 'lib/backbone', 'modules/touch',
     'lib/persistence', 'lib/persistence.store.sql', 'lib/persistence.store.websql',
+    'lib/async'
   ],
   function() {
     window.B = Backbone;
