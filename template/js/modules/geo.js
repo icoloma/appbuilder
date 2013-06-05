60c60de875b905496b282ea00181deed2790d549
José Roberto Vidal <rvidal@extrema-sistemas.com>
2013-06-05 10:55:22 -0400

    Extiende el API de persistence.QueryCollection, añade geo.js   * asJSON, asCollection   * geo.js: operaciones comunes con puntos geolocalizados

---
+++ template/js/modules/geo.js
@@ -0,0 +1,69 @@
+define(function() {
+  
+  // Longitud 'normalizada'.
+  // Supone que @lat viene en grados. Devuelve en las mismas unidades que @lon. 
+  var normalizeLon = function(lat, lon) {
+    return Math.sin( lat / 180 * Math.PI) * lon;
+  }
+  , earthRadius = 6370
+  ;
+
+
+  /*
+    Métodos utiles para geoposicionamiento
+    NO son aptos cerca de los polos o del antimeridiano 180º.
+    El error en las distancias es despreciable para su uso en España
+  */
+  return {
+    // Calcula un valor proporcional al cuadrado de la distancia. 
+    // Apto para comparar, ahorra operaciones 
+    propDistance: function(lat1, lon1, lat2, lon2) {
+      var normLon1 = normalizeLon(lat1, lon1)
+      , normLon2 = normalizeLon(lat2, lon2)
+      ;
+      return Math.pow(lat1-lat2, 2) + Math.pow(normLon1 - normLon2, 2);
+    },
+    // Distancia entre dos puntos en kilómetros.
+    // Todos los argumentos deben ir en grados.
+    distance: function(lat1, lon1, lat2, lon2) {
+      var normLon1 = normalizeLon(lat1, lon1) * Math.PI / 180
+      , normLon2 = normalizeLon(lat2, lon2) * Math.PI / 180
+      ;
+      return earthRadius * Math.sqrt(
+        Math.pow((lat1-lat2)/180*Math.PI, 2) + Math.pow(normLon1 - normLon2, 2)
+      ); 
+    },
+    // Filtro para buscar por distancia. 
+    // @lat y @lon deben ir en grados, @distance en kilometros
+    distanceFilter: function(lat, lon, distance) {
+      var normLon = normalizeLon(lat, lon)
+      , angle = 180 * distance / (2 * Math.PI * earthRadius)
+      , minLat = lat - angle
+      , maxLat = lat + angle
+      , minNormLon = normLon - angle
+      , maxNormLon = normLon + angle
+      ;
+
+      return function(latitude, normalizedLon) {
+        return latitude > minLat &&
+                latitude < maxLat &&
+                normalizedLon > minNormLon &&
+                normalizedLon < maxNormLon;
+      };
+    }
+    // // Cotas para el filtro por distancia. Las coordenadas deben ir en grados.
+    // bounds: function(lat, lon, distance) {
+    //   var normLon = normalizeLon(lat, lon)
+    //   , angle = 180 * distance / (2 * Math.PI * earthRadius) 
+    //   ;
+
+    //   return {
+    //     'min-lat': lat - angle,
+    //     'max-lat': lat + angle,
+    //     'min-normLon': normLon - angle,
+    //     'max-normLon': normLon + angle,
+    //   };
+    // }
+
+  };
+});
--- template/js/modules/query.js
+++ template/js/modules/query.js
@@ -1,12 +1,47 @@
-define(['lib/persistence'], function() {
-  // QueryCollection.query permite especificar varios filtros 
-  // en un solo objeto @queryObject
-  // OJO: de momento solo parseamos filtros '='
-  persistence.QueryCollection.prototype.query = function(queryObject) {
-    var queryCollection = this;
-    for (var field in queryObject) {
-      queryCollection = queryCollection.filter(field, '=', queryObject[field]);
+define(['globals'], function() {
+
+  // var minRegex = /^min-(.+)/
+  // , maxRegex = /^max-(.+)/
+  // ;
+
+  _.extend(persistence.QueryCollection.prototype, {
+    /*
+      QueryCollection.query permite especificar varios filtros en un solo objeto @queryObject
+      @queryObject: un hash de filtros nombre/valor. Por defecto aplica un filtro '='.
+
+    */
+    query: function(queryObject) {
+      var queryCollection = this
+      // , fieldName
+      ;
+      for (var field in queryObject) {
+        // if (field.match(minRegex) && (fieldName = field.match(minRegex)[1])) {
+        //   queryCollection = queryCollection.filter(fieldName, '>=', queryObject[field]);
+        // } else if (field.match(maxRegex) && (fieldName = field.match(maxRegex)[1])) {
+        //   queryCollection = queryCollection.filter(fieldName, '<=', queryObject[field]);
+        // } else {
+        queryCollection = queryCollection.filter(field, '=', queryObject[field]);
+        // }
+      }
+      return queryCollection;
+    },
+
+    asJSON: function(callback) {
+      this.list(function(results) {
+        callback(results.map(function(item) {
+          return item.toJSON();
+        }));
+      });
+    },
+
+    asCollection: function(callback) {
+      this.list(function(results) {
+        callback(new B.Collection(
+          results.map(function(item) {
+            return new B.Model(item.toJSON());
+          })
+        ));
+      });
     }
-    return queryCollection;
-  };
+  });
 });
