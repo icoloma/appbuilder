3ace272c5fc7afa224d3be8db082f0f215084e26
Ernesto Hdez <ehdez@extrema-sistemas.com>
2013-06-18 13:07:36 +0200

    Fixed #35

--- openCatalog/src/main/java/info/spain/opencatalog/config/DataRestConfig.java
+++ openCatalog/src/main/java/info/spain/opencatalog/config/DataRestConfig.java
@@ -1,23 +1,53 @@
 package info.spain.opencatalog.config;
 
+import java.io.IOException;
+
+import org.joda.time.DateTime;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 
-@Configuration
-@Import( ApiMvcConfig.class)
-@ComponentScan( basePackages="info.spain.opencatalog.rest" )
-public class DataRestConfig extends RepositoryRestMvcConfiguration  {
-	
-	
-//  FIXME: No lo registra bien, hay que usar @ConvertWith en los métodos de PoiRepository
-//	@Override
-//	protected void configureConversionService(ConfigurableConversionService conversionService) {
-//		conversionService.addConverter(String[].class, Poi.class, new PointConverter());
-//		conversionService.addConverter(String[].class, Distance.class, new DistanceConverter());
-//	}
-	
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.module.SimpleSerializers;
 
-	
+@Configuration
+@Import(ApiMvcConfig.class)
+@ComponentScan(basePackages = "info.spain.opencatalog.rest")
+public class DataRestConfig extends RepositoryRestMvcConfiguration {
+
+	@Override
+	protected void configureJacksonObjectMapper(ObjectMapper objectMapper) {
+		objectMapper.registerModule(new SimpleModule("MyCustomModule") {
+			private static final long serialVersionUID = -786299906589974831L;
+
+			@Override
+			public void setupModule(SetupContext context) {
+				SimpleSerializers serializers = new SimpleSerializers();
+				serializers.addSerializer(DateTime.class, new CustomDateTimeSerializer());
+				context.addSerializers(serializers);
+				
+			}
+		});
+	}
+
+	public class CustomDateTimeSerializer extends JsonSerializer<DateTime> {
+
+		@Override
+		public Class<DateTime> handledType() {
+			return DateTime.class;
+		}
+
+		@Override
+		public void serialize(DateTime dateTime, JsonGenerator jgen, SerializerProvider provider) throws IOException, JsonProcessingException {
+			jgen.writeString(dateTime.toString()); // ISO8601
+		}
+	}
+
+
 }
