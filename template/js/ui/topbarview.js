9a489bc164e7c75b9f84ab541b9b62bca2132725
José Roberto Vidal <rvidal@extrema-sistemas.com>
2013-08-16 11:16:47 +0200

    Arregla títulos en la topbar

--- template/js/ui/topbarview.js
+++ template/js/ui/topbarview.js
@@ -19,21 +19,44 @@ define(['globals'], function() {
           this.trigger(action);
           if (async) this.block($target);
         }
-      }
+        this.toggleMenu();
+      },
+      'tap .menu-button': 'toggleMenu'
     },
 
     tmpl: _.template(
       '<div class="navbar">' +
         '<span class="<% if (!root) { %>back-button icon-back<% } %> bar-button"></span>' +
-        '<h1 class="pagetitle">{{title}}</h1>' +
+        '<h1 class="pagetitle hideable">{{title}}</h1>' +
         // '<span class="options-button">Options</span>'
       '</div>' +
       '<div class="actionbar">' +
-        '<% if (search) { %><span data-action="search" data-async="true" class="titlesize search-button icon-search bar-button"></span><% } %>' +
-        '<% if (filter) { %><span data-action="filter" data-async="true" class="titlesize filter-button icon-filter bar-button"></span><% } %>' +
-        '<% if (sort) { %><span data-action="sort" data-async="true" class="titlesize sort-button icon-sort bar-button"></span><% } %>' +
-        '<% if (notify) { %><span data-action="notify" class="titlesize notify-button">Notify</span><% } %>' +
-        '<% if (map) { %><span data-action="map" class="titlesize map-button bar-button icon-map"></span><% } %>' +
+        '<% if (search) { %>' +
+          '<span data-action="search"' +
+            'data-async="true" class="titlesize search-button icon-search bar-button">' +
+          '</span>' +
+        '<% } else {%>' +
+          '<span class="bar-button icon-menu menu-button"></span>' +
+          '<div class="action-menu">' +
+            '<% if (filter) { %>' +
+              '<span data-action="filter"' +
+                'data-async="true" class="titlesize filter-button icon-filter bar-button">' +
+              '</span>' +
+            '<% } %><% if (sort) { %>' +
+              '<span data-action="sort"' +
+                'data-async="true" class="titlesize sort-button icon-sort bar-button">' +
+              '</span>' +
+            '<% } %><% if (notify) { %>' +
+              '<span data-action="notify"' +
+                'class="titlesize notify-button">Notify' +
+              '</span>' +
+            '<% } %><% if (map) { %>' +
+              '<span data-action="map"' +
+                'class="titlesize map-button bar-button icon-map">' +
+              '</span>' +
+            '<% } %>' +
+          '</div>' +
+        '<% } %>' +
       '</div>'
     ),
 
@@ -62,7 +85,9 @@ define(['globals'], function() {
     unblock: function() {
       this.blocked$El.removeClass('busy');
       this.blocked$El = false;
+    },
+    toggleMenu: function() {
+      this.$('.action-menu').toggle();
     }
-
   });
 });
