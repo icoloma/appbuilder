6aa4fea98ba749058b866f5f525d9e0e9968dd5c
Ernesto Hdez <ehdez73@gmail.com>
2013-10-08 14:40:07 +0200

    Fixed #78

--- openCatalog/src/main/webapp/resources/js/user/user-zones.js
+++ openCatalog/src/main/webapp/resources/js/user/user-zones.js
@@ -15,14 +15,14 @@ $(function(){
 		e && e.preventDefault();
 		var id = $('#addZone').val();
 		var name = $('#addZone option:selected').text();
-		
-			
-	//	<td><span>Tenerife</span><input type="hidden"  class="idZone" name="idZones[0]" value="1"/></td>
-		
-		
+		var link = $('table.zones').data('link') + id;
 		var tr = $('<tr/>').appendTo("table.zones > tbody");
 		var td1 = $('<td/>').appendTo(tr);
-		var span = $('<span/>').html(name).appendTo(td1);
+		var span = $('<span/>')
+					.addClass('dataLink')
+					.click( function(){window.location= link;})
+					.attr('data-link',link)
+					.html(name).appendTo(td1);
 		var input = $('<input/>').attr("type","hidden").attr("value", id).appendTo(td1).attr("class","idZone");
 		
 		var td2 = $('<td/>').appendTo(tr);
