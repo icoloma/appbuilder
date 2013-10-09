366f37e61ee05765d40770013c8558ccea9b8493
José Roberto Vidal <rvidal@extrema-sistemas.com>
2013-10-09 12:54:08 -0400

    Espera a DocumentReady para escuchar taps

--- template/js/ui/touch.js
+++ template/js/ui/touch.js
@@ -4,12 +4,16 @@
 */
 define(['hammer', 'ui/pageanimation', 'ui/activable'], function() {
 
-  $(document)
-    .hammer()
-    .on('tap.checkable', '.checkable', function(e) {
-      var $el = $(e.currentTarget).find('[type="checkbox"], [type="radio"]');
-      $el.prop('checked', !$el.prop('checked'));
-    })
-  ;
+
+  $(function() {
+    $(document)
+      .hammer()
+      .on('tap.checkable', '.checkable', function(e) {
+        var $el = $(e.currentTarget).find('[type="checkbox"], [type="radio"]');
+        $el.prop('checked', !$el.prop('checked'));
+      })
+    ;
+
+  });
 
 });
